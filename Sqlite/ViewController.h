473abdafc79f7172ac3ad4d57ad04d190490c92e
benlu <benlu@benlutekiMacBook-Pro.local>
2014-01-29 11:12:24 +0800

    Initial Commit

---
+++ Sqlite/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sqlite
+//
+//  Created by benlu on 1/29/14.
+//  Copyright (c) 2014 benlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SqliteTests/SqliteTests.m
@@ -0,0 +1,34 @@
+//
+//  SqliteTests.m
+//  SqliteTests
+//
+//  Created by benlu on 1/29/14.
+//  Copyright (c) 2014 benlu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SqliteTests : XCTestCase
+
+@end
+
+@implementation SqliteTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
